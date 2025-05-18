eefb675f2965accd4355974df21d5bfb638303d8
Sacha (Pc) <S.soulslasher@gmail.com>
2025-05-18 16:56:49 +0200

    [ADD] Setup Gym for testing

--- Assets/Game/Scripts/Player/Movement.cs
+++ Assets/Game/Scripts/Player/Movement.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] Camera _playerCamera;
+    [SerializeField] private Camera playerCamera;
+
+    [SerializeField] private float speed = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,23 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.)
+        Vector3 cameraDir = new Vector3(playerCamera.transform.forward.x, 0, playerCamera.transform.forward.z);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += speed * Time.deltaTime * cameraDir; 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += speed * Time.deltaTime * -cameraDir; 
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += speed * Time.deltaTime * (Quaternion.AngleAxis(-90,Vector3.up)*cameraDir); 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += speed * Time.deltaTime * (Quaternion.AngleAxis(90,Vector3.up)*cameraDir); 
+        }
     }
 }
